a5815b679c29960ba651d1c3ab474503d6fe31f8
Maarten van Stam <maarten@vanstam.org>
2018-09-11 14:09:21 +0200

    Add project files.

---
+++ src/dutchdotnetgroup.nl/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved. 
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace System
+{
+    public static class DateTimeExtensions
+    {
+        private const string CEST = "Central Europe Standard Time";
+        private static readonly TimeZoneInfo _cetTimeZone = TimeZoneInfo.FindSystemTimeZoneById(CEST);
+
+        public static DateTime ConvertToTimeZone(this DateTime dateTime, TimeZoneInfo sourceTimeZone, TimeZoneInfo destinationTimeZone)
+        {
+            return TimeZoneInfo.ConvertTime(dateTime, sourceTimeZone, destinationTimeZone);
+        }
+
+        public static DateTime ConvertFromUtcToCet(this DateTime dateTime)
+        {
+            return dateTime.ConvertToTimeZone(TimeZoneInfo.Utc, _cetTimeZone);
+        }
+
+        public static DateTime ConvertFromCetToUtc(this DateTime dateTime)
+        {
+            return dateTime.ConvertToTimeZone(_cetTimeZone, TimeZoneInfo.Utc);
+        }
+    }
+}
---
+++ src/dutchdotnetgroup.nl/Services/ShowList.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved. 
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using dutchdotnetgroup.nl.Models;
+
+namespace dutchdotnetgroup.nl.Services
+{
+    public class ShowList
+    {
+        public IList<Show> PreviousShows { get; set; }
+
+        public string MoreShowsUrl { get; set; }
+    }
+}
